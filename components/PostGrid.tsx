11b7949e6493cc9b7c4cb73b82dcefe429f5ea4d
Hajanirina Ridjvan RANDRIANANDRAINA <darijavan@gmail.com>
2026-06-01 14:26:53 +0300

    chore: add spacing on the edges for mobile

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -87,7 +87,7 @@ export function PostGrid({ username, initialPosts, initialNextCursor }: PostGrid
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-1">
+      <div className="grid grid-cols-3 gap-1 p-1 md:p-0">
         {posts.map((post) => (
           <a
             key={post.instagramPostId}
